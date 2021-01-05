441ee4a932e49dcd53cd320cf3a564c8c0c97f59
sunkanmiadewumi@gmail.com <sunkanmiadewumi1@gmail.com>
2021-01-05 15:50:01 +0100

    lastest commit

---
+++ connetct-kafka.js
@@ -0,0 +1,22 @@
+const {Kafka} = require('kafkajs')
+const kafka = new Kafka({
+    clientId:'app-id',
+    brokers:['localhost:9092']  
+})
+const topicName= "createOther"
+const process = async ()=>{
+   const admin = kafka.admin()
+   await  admin.connect()
+   await  admin.createTopics({
+       topics: [
+        {
+            topic: topicName,
+            numOfPartitions:2,
+            replicationFactor: 1
+        }
+    ]
+   })
+   await  admin.disconnect()
+}
+
+process().then(()=> console.log('Kafka Connected'))
